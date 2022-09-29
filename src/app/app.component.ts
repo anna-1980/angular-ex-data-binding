69d95afcff32f6f4a4ffc06dcdc880d721328d04
Anna Zdyb <anna.zdyb@gmail.com>
2022-09-29 16:55:59 +0200

    getting deeper into data binding events ets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ColdObservable } from 'rxjs/internal/testing/ColdObservable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'], 
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'excersizes-65';
+  serverElements: any[] = [
+{type: 'server', name: 'testserver', content: 'Just a test! hard coded in app.component.ts'}
+  ];
+
+// -------- lecture 69 --------------//
+  onServerAdded( serverData: {serverName: string, serverContent: string}) {
+    console.log('addServer')
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    console.log('addBluePrint')
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
 }
