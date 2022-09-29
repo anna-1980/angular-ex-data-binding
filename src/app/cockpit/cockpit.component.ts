e5cfd4fa6c9d1df216ee590b5760f131a1842278
Anna Zdyb <anna.zdyb@gmail.com>
2022-09-29 17:00:23 +0200

    local references

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,7 +10,8 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated= new EventEmitter<{serverName: string, serverContent: string}>();
 //------- you can assign alias to the @Output -------//
   @Output('bpCreated') blueprintCreated =new EventEmitter<{serverName: string, serverContent: string}>();
-  newServerName = '';
+  // newServerName = ''; --- it is possible to comment it out becaue we get the value form the local reference on input
+  // remember to add in HTML (click)="onAddBlueprint(serverNameInput)
   newServerContent = '';
 
   constructor() { }
@@ -19,17 +20,17 @@ export class CockpitComponent implements OnInit {
   }
 
 
-  onAddServer(nameInput) {
+  onAddServer(nameInput: HTMLInputElement) {
     console.log(nameInput)
     console.log(nameInput.value)
    this.serverCreated.emit({
-    serverName: this.newServerName, 
+    serverName: nameInput.value, 
     serverContent: this.newServerContent});
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName, 
+      serverName: nameInput.value, 
       serverContent: this.newServerContent});
   }
 
